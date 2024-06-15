b793c84e3363f7eba8e70a6ca7a825d9d77fb5f8
shun2222 <shun.su.jp@gmail.com>
2024-06-15 10:39:52 +0900

    update: render

--- open_spiel/python/mfg/gif_maker.py
+++ open_spiel/python/mfg/gif_maker.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 from gif_maker import *
+from sklearn.neighbors import KernelDensity
 
 plt.rcParams["animation.ffmpeg_path"] = r"/usr/bin/ffmpeg"
 
-def multi_render(datas, filename, labels, vmin=None, vmax=None, cmap='viridis'):
+def multi_render(datas, filename, labels, vmin=None, vmax=None, cmap='viridis', use_kde=False):
 
     n_datas = len(datas)
 
@@ -19,6 +20,8 @@ def multi_render(datas, filename, labels, vmin=None, vmax=None, cmap='viridis'):
         for i in range(n_datas):
             axes[i].tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False, bottom=False, left=False, right=False, top=False)
             im = axes[i].imshow(datas[i][t], animated=True, vmin=vmin, vmax=vmax, cmap=cmap) 
+            X, Y, Z, _ = calc_kde(data[i][t])
+            im = axes[i].contour(X+0.5, Y+0.5, Z, 10)
             if t==0 and i==n_datas-1:
                 axes[n_datas].axis('off')
                 fig.colorbar(im, ax=axes[n_datas])
@@ -108,3 +111,47 @@ class GifMaker():
     def reset(self):
         plt.close()
         self.datas = []
+
+def calc_kde(prob_datas, num_agent=1000):
+    datas = np.array(prob_datas)
+    d_shape = n_data.shape
+    assert len(d_shape)==2, f'shape error in kde plot {d_shape}'
+
+    n_data = (datas*1000).astype(np.int64)
+
+    # 2次元の確率データの例（仮のデータ）
+    #data = np.random.randn(1000, 2)  # 1000個の2次元データポイント
+    data = []
+    for r in range(d_shape[0]):
+        for c in range(d_shape[1]):
+            data += [[r, c] for i in range(n_data[r, c])]
+
+    n_data = np.zeros(d_shape)
+
+    # カーネル密度推定を使用してPDFを推定
+    kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(data)
+
+    # 等高線プロットの準備
+    x = y = np.array([i for i in range(d_shape[0])]) 
+    X, Y = np.meshgrid(x, y)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+
+    # 確率密度関数の計算
+    Z = np.exp(kde.score_samples(xy))
+    Z = Z.reshape(X.shape)
+    Z2 = []
+    for i in range(len(Z)):
+        Z2.append(Z[len(Z)-i-1])
+    Z = Z2
+    return X, Y, Z, n_data
+
+    ## 等高線プロット4
+    #plt.figure(figsize=(8, 6))
+    #plt.imshow(n_data,  extent=[0, d_shape[0], 0, d_shape[1]], alpha=0.3)
+    #heatmap = plt.imshow(Z, extent=[0, d_shape[0], 0, d_shape[1]], origin='lower', cmap='viridis', alpha=0.8)
+    #plt.contour(X+0.5, Y+0.5, Z, 10)
+    #plt.xlabel('X')
+    #Jplt.ylabel('Y')
+    #plt.title('2D Probability Density Function')
+    #plt.legend()
+    #plt.show()
